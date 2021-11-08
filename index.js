a38bd20ddda3a92a5147a2c964f846ed5608638d
twinh <twinhuang@qq.com>
2021-11-08 23:33:02 +0800

    feat(a-input): `Input` 组件支持显示字数，`Input.TextArea` 显示字数在输入框内

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+import './index.less';
 import {Input as BaseInput} from 'antd';
 
-const Input = (props) => {
-  return <BaseInput
-    {...props}
-  />
+const Counter = ({showCount = true, ...props}) => {
+  if (!showCount || !props.maxLength) {
+    return null;
+  }
+  return <span className="ant-input-show-count">{(props?.value?.length || 0) + ' / ' + props.maxLength}</span>;
 };
+
+const Input = ({showCount = true, ...props}) => {
+  return <BaseInput
+    suffix={<Counter showCount={showCount} {...props}/>}
+    {...props}
+  />;
+};
+
+Input.TextArea = ({showCount = true, ...props}) => <BaseInput.TextArea showCount={showCount} {...props}/>;
 
 export default Input;
