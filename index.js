ccd76d837b5eab875a9ade6fd68e73f9d571026e
twinh <twinhuang@qq.com>
2021-11-16 20:34:52 +0800

    feat(a-input): 增加自动去空格功能

--- index.js
+++ index.js
@@ -8,13 +8,25 @@ const Counter = ({showCount = true, ...props}) => {
   return <span className="ant-input-show-count">{(props?.value?.length || 0) + ' / ' + props.maxLength}</span>;
 };
 
-const Input = ({showCount = true, ...props}) => {
+const handleBlur = (trim, props, e) => {
+  trim && props.onChange && props.onChange(e.target.value.trim());
+  props.onBlur && props.onBlur(e);
+};
+
+const Input = ({showCount = true, trim = true, ...props}) => {
   return <BaseInput
     suffix={<Counter showCount={showCount} {...props}/>}
+    onBlur={handleBlur.bind(this, trim, props)}
     {...props}
   />;
 };
 
-Input.TextArea = ({showCount = true, ...props}) => <BaseInput.TextArea showCount={showCount} {...props}/>;
+Input.TextArea = ({showCount = true, trim = true, ...props}) => {
+  return <BaseInput.TextArea
+    showCount={showCount}
+    onBlur={handleBlur.bind(this, trim, props)}
+    {...props}
+  />;
+};
 
 export default Input;
