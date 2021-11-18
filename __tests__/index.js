15d655233762002ffc4502cffa4ff0e851559fa9
twinh <twinhuang@qq.com>
2021-11-18 11:05:27 +0800

    test(a-input): 增加 `showCount` 测试

--- __tests__/index.js
+++ __tests__/index.js
@@ -7,4 +7,14 @@ describe('Input', () => {
 
     expect(result.container).toMatchSnapshot();
   });
+
+  test('showCount without value', () => {
+    const result = render(<Input maxLength={10}/>);
+    expect(result.container).toMatchSnapshot();
+  });
+
+  test('showCount with value', () => {
+    const result = render(<Input maxLength={10} value="abc"/>);
+    expect(result.container).toMatchSnapshot();
+  });
 });
