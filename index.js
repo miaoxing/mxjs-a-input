f0e726cee29169bf64f1109460f68c5232e5359b
twinh <twinhuang@qq.com>
2022-01-08 11:04:39 +0800

    feat(a-input): 增加 prop-types 校验

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import './index.less';
 import {Input as BaseInput} from 'antd';
+import PropTypes from 'prop-types';
 
 const Counter = ({showCount = true, ...props}) => {
   if (!showCount || !props.maxLength) {
     return null;
   }
   return <span className="ant-input-show-count">{(props?.value?.length || 0) + ' / ' + props.maxLength}</span>;
+};
+
+Counter.propTypes = {
+  showCount: PropTypes.bool,
+  maxLength: PropTypes.number,
+  value: PropTypes.string,
 };
 
 const handleBlur = (trim, props, e) => {
@@ -21,6 +28,11 @@ const Input = ({showCount = true, trim = true, ...props}) => {
   />;
 };
 
+Input.propTypes = {
+  showCount: PropTypes.bool,
+  trim: PropTypes.bool,
+};
+
 Input.TextArea = ({showCount = true, trim = true, ...props}) => {
   return <BaseInput.TextArea
     showCount={showCount}
@@ -28,5 +40,7 @@ Input.TextArea = ({showCount = true, trim = true, ...props}) => {
     {...props}
   />;
 };
+
+Input.TextArea.propTypes = Input.propTypes;
 
 export default Input;
