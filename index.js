b6983503bb00d4f9c04b2986d407ea1e787c9475
twinh <twinhuang@qq.com>
2023-01-29 19:19:55 +0800

    feat(a-input): `antd` `4.18` 显示字数改用默认的 `showCount` 属性
    同时 less 简化为 css

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
-import './index.less';
+import './index.css';
 import {Input as BaseInput} from 'antd';
 import PropTypes from 'prop-types';
-
-const Counter = ({showCount = true, ...props}) => {
-  if (!showCount || !props.maxLength) {
-    return null;
-  }
-  return <span className="ant-input-show-count">{(props?.value?.length || 0) + ' / ' + props.maxLength}</span>;
-};
-
-Counter.propTypes = {
-  showCount: PropTypes.bool,
-  maxLength: PropTypes.number,
-  value: PropTypes.string,
-};
 
 const handleBlur = (trim, props, e) => {
   trim && props.onChange && props.onChange(e.target.value.trim());
   props.onBlur && props.onBlur(e);
 };
 
-const Input = ({showCount = true, trim = true, ...props}) => {
+const Input = ({maxLength = null, trim = true, ...props}) => {
   return <BaseInput
-    suffix={<Counter showCount={showCount} {...props}/>}
+    maxLength={maxLength}
+    showCount={!!maxLength}
     onBlur={handleBlur.bind(this, trim, props)}
     {...props}
   />;
 };
 
 Input.propTypes = {
-  showCount: PropTypes.bool,
+  maxLength: PropTypes.number,
   trim: PropTypes.bool,
 };
 
